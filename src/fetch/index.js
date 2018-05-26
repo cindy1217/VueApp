93e08dc12816276974e4ae68e58f05929bd1adb1
xiaoping <1079075924@qq.com>
2018-05-26 17:55:00 +0800

    合并

--- src/fetch/index.js
+++ src/fetch/index.js
@@ -8,6 +8,7 @@ export default (Vue) => {
   // 增加一个拦截器，当method为form时，使用表单提交的方式
   // 使用qs包将data转为表单数据
   axios.interceptors.request.use((config) => {
+  	console.log(config,'我我我')
     if (config.method === 'form') {
       config.method = 'post'
       config.data = qs.stringify(config.data)
