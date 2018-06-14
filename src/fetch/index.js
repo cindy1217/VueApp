f1b9b00c057de42f99a72193761867861b49bfee
Toyuye <xiaoping.tao@molbase.com>
2018-06-14 17:55:27 +0800

    调增fetch

--- src/fetch/index.js
+++ src/fetch/index.js
@@ -1,8 +1,5 @@
 import axios from 'axios'
 import qs from 'qs'
-import prodEnv from '../../config/prod.env'
-import devEnv from '../../config/dev.env'
-
 export default (Vue) => {
   // 使用axios来发ajax
   // 增加一个拦截器，当method为form时，使用表单提交的方式
@@ -14,21 +11,8 @@ export default (Vue) => {
       config.data = qs.stringify(config.data)
       config.headers['Content-Type'] = 'application/x-www-form-urlencoded'
     }
-    // 控制环境配置，npm run dev 开发环境 ，npm run build 生成环境
-    let envConfig 
-    let _NODE_ENV = process.env.NODE_ENV 
-    switch(_NODE_ENV){
-      case 'development':
-        envConfig = devEnv;
-      break;
-      case 'production':
-        envConfig = prodEnv;
-      break;
-      default:
-        envConfig = devEnv;
-    }
     // 调用公共的请求路径
-    config.baseURL = envConfig.HTTP.BASE_URL
+    config.baseURL = process.env.HTTP.BASE_URL
     // 设置超时时间
     config.timeout = 10000;
     // 增加时间戳，防止拉取缓存
