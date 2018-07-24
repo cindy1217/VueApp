f179b50cc72bf5bba853c045426117297a1add5b
Toyuye <xiaoping.tao@molbase.com>
2018-07-24 17:28:41 +0800

    增加分享js但未处理

--- src/main.js
+++ src/main.js
@@ -4,14 +4,20 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './vuex/store'
+// 判断系统
+import { OS } from './utils'
+
 
 import "./assets/sass/common/_global.scss";
 import "./assets/sass/common/_function.scss";
 // 前置守卫
 router.beforeEach((to, from, next) => {
   // console.log(to,from,next,'我都')
-  console.log(to)
-  next()
+  // 兼容 ios 微信分享功能
+  if (OS().ios && to.path !== location.pathname) {
+    // 此处不可使用location.replace
+    location.assign(to.fullPath)
+  } 
 })
 
 // 后置守卫
@@ -23,10 +29,7 @@ import fetch from './fetch'
 // 挂载到原型上
 Vue.prototype.$http = fetch
 
-// 引入公用方法库
-import Utils from './utils'
-// 将公用方法定义在vue原型
-Vue.prototype.Utils = Utils
+
 
 Vue.config.productionTip = false
 
